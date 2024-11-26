e2fc74984988736132f02e40921c49b03dc39b96
dongminlee94 <kid33629@gmail.com>
2024-11-26 11:22:43 +0900

    add requester module

---
+++ src/interface/requester.py
@@ -0,0 +1,43 @@
+"""Requester."""
+
+from requests.adapters import HTTPAdapter, Retry
+from requests.sessions import Session
+
+
+def requests_retry_session(
+    max_retries: int = 10,
+    backoff_factor: int = 5,
+    status_forcelist: list[int] = [400, 403, 429, 500, 502, 503, 504],
+) -> Session:
+    """
+    Create a requests session with retry logic.
+
+    :param max_retries: Maximum number of retries for failed requests.
+    :param backoff_factor: Factor for exponential backoff strategy.
+    :param status_forcelist: List of HTTP status codes to trigger a retry.
+    :returns: A configured requests Session object.
+    :note: If we do not receive the correct response when we send the request,
+            retry as much as max_retries.
+            We also use an "exponential backoff" strategy,
+            where the delay is "backoff_factor * (2 ^ (retry count - 1))" after each failed retry.
+
+    Example:
+        >>> session = requests_retry_session(max_retries=5, backoff_factor=2)
+        >>> response = session.get("https://example.com")
+        >>> print(response.status_code)
+    """
+    # Configure retry strategy
+    retry = Retry(
+        total=max_retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+
+    # Create an HTTP adapter with the retry strategy
+    adapter = HTTPAdapter(max_retries=retry)
+
+    # Create and configure the session
+    session = Session()
+    session.mount(prefix="http://", adapter=adapter)
+
+    return session
